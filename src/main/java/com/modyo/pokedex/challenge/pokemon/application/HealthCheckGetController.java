7265a301ba9645888f4ee684b876bcb812192863
Efrain Garay <egaray7@gmail.com>
2022-04-18 12:45:08 -0400

    feat(): Healcheck controller and aceptacion testing

--- src/main/java/com/modyo/pokedex/challenge/pokemon/application/HealthCheckGetController.java
+++ src/main/java/com/modyo/pokedex/challenge/pokemon/application/HealthCheckGetController.java
@@ -1,2 +1,16 @@
-package com.modyo.pokedex.challenge.pokemon.application;public class HealthCheckGetController {
+package com.modyo.pokedex.challenge.pokemon.application;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HealthCheckGetController {
+    @GetMapping("/status")
+    @ResponseStatus(HttpStatus.OK)
+    public void ok() {
+
+    }
 }
--- src/test/java/com/modyo/pokedex/challenge/pokemon/application/HealthCheckGetControllerStep.java
+++ src/test/java/com/modyo/pokedex/challenge/pokemon/application/HealthCheckGetControllerStep.java
@@ -1,4 +1,46 @@
-package com.modyo.pokedex.challenge.pokemon.infrastucture;
+package com.modyo.pokedex.challenge.pokemon.application;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class HealthCheckGetControllerStep {
+    private final static String BASE_URI = "http://localhost";
+
+    @LocalServerPort
+    private int port;
+
+    private ValidatableResponse validatableResponse;
+
+    private void configureRestAssured() {
+        RestAssured.baseURI = BASE_URI;
+        RestAssured.port = port;
+    }
+
+
+    protected RequestSpecification requestSpecification() {
+        configureRestAssured();
+        return given();
+    }
+
+    @Given("I send a request to the URL {string}")
+    public void iSendARequest(String endpoint) throws Throwable {
+        validatableResponse = requestSpecification().contentType(ContentType.JSON)
+                .when().get(endpoint).then();
+    }
+
+    @Then("the response will return status {int}")
+    public void extractResponse(int status) {
+        validatableResponse.assertThat().statusCode(equalTo(status));
+    }
 }
