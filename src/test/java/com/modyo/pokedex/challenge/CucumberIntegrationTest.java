58be9622cadc1a283ef61c3bfd90ce1a37e0a4b6
Efrain Garay <egaray7@gmail.com>
2022-04-18 12:42:43 -0400

    chore(cumunber):  cucumber config

--- src/test/java/com/modyo/pokedex/challenge/CucumberIntegrationTest.java
+++ src/test/java/com/modyo/pokedex/challenge/CucumberIntegrationTest.java
@@ -1,2 +1,12 @@
-package com.modyo.pokedex.challenge;public class CucumberIntegrationTest {
+package com.modyo.pokedex.challenge;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty",features = "src/test/resources/features/",publish = true,glue = {
+        "com.modyo.pokedex.challenge"})
+public class CucumberIntegrationTest {
+
 }
--- src/test/java/com/modyo/pokedex/challenge/CucumberSpringConfiguration.java
+++ src/test/java/com/modyo/pokedex/challenge/CucumberSpringConfiguration.java
@@ -1,2 +1,15 @@
-package com.modyo.pokedex.challenge;public class CucumberSpringConfiguration {
+package com.modyo.pokedex.challenge;
+
+import com.modyo.pokedex.challenge.pokemon.application.HealthCheckGetController;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = {ChallengeApplication.class, HealthCheckGetController.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = {ChallengeApplication.class, HealthCheckGetController.class}, loader = SpringBootContextLoader.class)
+public class CucumberSpringConfiguration {
+
 }
