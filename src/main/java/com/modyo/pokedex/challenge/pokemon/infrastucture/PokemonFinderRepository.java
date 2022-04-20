f57e30db5d27983447ada84996c96432ef8f9725
Efrain Garay <egaray7@gmail.com>
2022-04-19 23:13:14 -0400

    chore(): deploy ecs aws

--- src/main/java/com/modyo/pokedex/challenge/pokemon/infrastucture/PokemonFinderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.modyo.pokedex.challenge.pokemon.infrastucture;
-
-import com.modyo.pokedex.challenge.pokemon.domain.PokemonResponseData;
-
-public interface PokemonFinderRepository {
-    PokemonResponseData finderPokemon(String name);
-}
